41952bd96a7b6bb896b1ee88ea85fb0051282e02
Gamepat <phyrofreaky@gmail.com>
2019-04-02 07:35:36 +0100

    Added lighttoggle

--- greenhouse.py
+++ greenhouse.py
@@ -1,27 +1,45 @@
 #!/usr/bin/python
 import thingspeak
 import time
+import RPi.GPIO as GPIO
 import Adafruit_DHT
+
+GPIO.setwarnings(False)
  
 channel_id = 694063
 write_key  = 'QNIM0VZ4VDTZKAW7'
 read_key   = 'XEMY9SF60QSDKH0R'
 pin = 4
 sensor = Adafruit_DHT.DHT11
+
+GPIO.setmode(GPIO.BCM)
+ 
+GPIO.setup(27, GPIO.OUT)
  
 def measure(channel):
-    try:
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-        # write
-        response = channel.update({'field1': temperature, 'field2': humidity})
-	print("Measured")
-    except:
-        print("connection failed")
+	try:
+        	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+	        # write
+	        response = channel.update({'field1': temperature, 'field2': humidity})
+		print("Measured")
+    	except:
+        	print("connection failed")
  
  
 if __name__ == "__main__":
-    channel = thingspeak.Channel(id=channel_id, write_key=write_key, api_key=read_key)
-    while True:
-        measure(channel)
-        # free account has an api limit of 15sec
-        time.sleep(15)
+    	channel = thingspeak.Channel(id=channel_id, write_key=write_key, api_key=read_key)
+   	while True:
+		dt = list(time.localtime())
+		hr = dt[3]
+		min = dt[4]
+		sec = dt[5]
+
+		if hr == 9 and min == 0:
+			GPIO.output(27, GPIO.LOW)
+		elif hr == 21 and min == 0:
+			GPIO.output(27, GPIO.HIGH)
+
+		measure(channel)
+        	# free account has an api limit of 15sec
+        	time.sleep(15)
+	GPIO.cleanup()
